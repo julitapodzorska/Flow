a0feea3192a821d228b8557baf1c0bfd389495f7
Julita Podżorska <podzorskajulita@gmail.com>
2019-08-05 22:56:40 +0200

    second commit

--- FlowControling/views.py
+++ FlowControling/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from django.http import HttpResponse
 from django.urls import reverse_lazy
@@ -7,5 +7,8 @@ from django.views import generic
 
 class LandingPage(View):
     def get(self, request):
-        return render(request, 'home.html')
+        if request.user.is_authenticated:
+            return render(request, 'home.html')
+        else:
+            return redirect('/accounts/login')
 
